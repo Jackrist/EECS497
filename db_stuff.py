56ce2fd0a04b73a7e75a4c015002d37037473f85
Dana Atoui <atouid@umich.edu>
2023-12-05 18:35:09 -0500

    css and html stuff

---
+++ db_stuff.py
@@ -0,0 +1,197 @@
+import sqlite3
+
+def init():
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    cur = con.cursor()
+
+    # Create a "users" table if necessary
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(50) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(100) NOT NULL
+        )
+    """) # Add more user-related fields
+
+    # Create a "lost_items" table if necessary
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS lost_items (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT,
+        item_name VARCHAR(100) NOT NULL,
+        description TEXT,
+        date_lost DATE,
+        location VARCHAR(100)
+        )
+    """) # Add more item-related fields
+
+        # Create a "found_items" table if necessary
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS found_items (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT,
+        item_name VARCHAR(100) NOT NULL,
+        description TEXT,
+        date_found DATE,
+        location VARCHAR(100)
+        )
+    """) # Add more item-related fields
+
+    # Create a "notification_preferences" table if necessary
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS notification_preferences (
+        user_id INT PRIMARY KEY,
+        item_pickup BOOLEAN,
+        item_dropoff BOOLEAN,
+        new_messages BOOLEAN
+        )
+    """) # Add more notification types
+
+    # Commit the changes and close the connection
+    con.commit()
+    con.close()
+
+
+# Helps track logged_in_user (Use on login page)
+def set_logged_in_user(username):
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    cur = con.cursor()
+
+    # Retrieve the user_id based on the username from the "users" table
+    cur.execute("SELECT id FROM users WHERE username = ?", (username))
+    user_id = cur.fetchone()
+
+    # Tracks current user
+    cur.execute("""DROP TABLE IF EXISTS [logged_in_user]
+                CREATE TABLE logged_in_user (
+                id INT PRIMARY KEY
+                )""")
+    cur.execute("""INSERT INTO logged_in_user
+                VALUES (?)""", (user_id))
+    
+    con.commit()
+    con.close()
+
+
+def get_logged_in_user():
+     # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    cur = con.cursor()
+
+    cur.execute("SELECT id FROM logged_in_user")
+    user_id = cur.fetchone()
+
+    cur.execute("""SELECT username FROM users
+                WHERE id = ?
+                """, (user_id))
+    logname = cur.fetchone()
+
+    con.commit()
+    con.close()
+
+    return logname, user_id
+
+
+def new_user(username, password, email):
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    # Creating a cursor makes a connection to connect theSQL server
+    cur = con.cursor()
+    cur.execute(""" 
+    INSERT INTO users
+    VALUES (""" + username + """, """ + password + """, """ + email + """ )
+    """)
+    con.commit()
+    con.close()
+
+
+def lost_item(user_id, item_name, description, date_lost, location):
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    # Creating a cursor makes a connection to connect the SQL server
+    cur = con.cursor()
+    cur.execute ("""
+    INSERT INTO lost_items
+    VALUES (""" + user_id + """, """ + item_name + """, """ + description + """, """ + date_lost + """,""" + location + """)
+    """)
+    con.commit()
+    con.close()
+    return 0
+
+
+def found_item(user_id, item_name, description, date_found, location):
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    # Creating a cursor makes a connection to connect the SQL server
+    cur = con.cursor()
+    cur.execute ("""
+    INSERT INTO lost_items
+    VALUES (""" + user_id + """, """ + item_name + """, """ + description + """, """ + date_found + """,""" + location + """)
+    """)
+    con.commit()
+    con.close()
+    return 0
+
+
+def set_preferences(user_id, item_pickup, item_dropoff, new_messages):
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    # Creating a cursor makes a connection to connect the SQL server
+    cur = con.cursor()
+    cur.execute ("""INSERT INTO notification_preferences
+                 VALUES (?, ?, ?, ?)
+                 """, (user_id, item_pickup, item_dropoff, new_messages))
+    con.commit()
+    con.close()
+
+
+def edit_preferences(user_id, item_pickup, item_dropoff, new_messages):
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    # Creating a cursor makes a connection to connect the SQL server
+    cur = con.cursor()
+    cur.execute ("""
+                 UPDATE notification_preferences
+                 SET item_pickup = ?, item_dropoff = ?, new_messages = ?
+                 WHERE user_id = ?
+                 """, (item_pickup, item_dropoff, new_messages, user_id))
+    con.commit()
+    con.close()
+
+def get_losts(user_id):
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    # Creating a cursor makes a connection to connect the SQL server
+    cur = con.cursor()
+    cur.execute ("""
+                 SELECT id, item_name, description, date_lost, location
+                 FROM lost_items WHERE user_id = ?
+                 """, (user_id))
+    
+    losts = cur.fetchall()
+
+    con.commit()
+    con.close()
+
+    return losts
+
+
+def get_founds(user_id):
+    # Connect to the database and create a cursor
+    con = sqlite3.connect("497_Lost_n_Found.db")
+    # Creating a cursor makes a connection to connect the SQL server
+    cur = con.cursor()
+    cur.execute ("""
+                 SELECT id, item_name, description, date_found, location
+                 FROM found_items WHERE user_id = ?
+                 """, (user_id))
+    
+    founds = cur.fetchall()
+
+    con.commit()
+    con.close()
+
+    return founds
---
+++ test.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+import sqlite3
+import db_stuff
+
+app = Flask(__name__)
+
+# Database initialization (if needed)
+db_stuff.db_init()
+
+# Define new (sometimes API ?) endpoints as needed below.
+
+@app.route('/signup/', methods=['POST'])
+def register_user():
+    '''Handles user registration & initial preference setting.'''
+    # 1. Grab html form values
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get ('email')
+   
+        # 2. (a) Call db_stuff func for adding new users
+        db_stuff.db_new_user(username, password, email)
+    # 4. Redirect or return a response
+        return jsonify({"message": "User registered uccessfully"})  
+    else:
+        return "Invalid request method"
+    
+@app.route('/preferences/')
+def set_preferences():
+    '''Set/update user notification preferences'''
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        item_pickup = request.form.get ('item_pickup')
+        item_dropoff = request.form.get ('item_dropoff')
+        new_messages = request.form.get ('new_messages')
+
+        # 2. (b) Retrieve user_id from "users" table
+        user_id = db_stuff.db_retrieve_user_id(username)
+        # 3. Call db_stuff func for setting preferences
+        db_stuff.db_edit_preferences(user_id, item_pickup, item_dropoff, new_messages)    # 2. Redirect?
+
+@app.route('/submitlost/')
+def submit_lost_item():
+    '''Adds a new item to the lost_items table.'''
+    # 1. Grab html form values
+    # 2. Call db_stuff func for adding new lost items
+    db_stuff.db_lost_item()
+    # 3. Redirect?
+
+@app.route('/submitfound/')
+def submit_found_item():
+    '''Adds a new item to the found_items table.'''
+    # 1. Grab html form values
+    # 2. Call db_stuff func for adding found items
+    db_stuff.db_found_item()
+    # 3. Redirect?
+
+@app.route('/')
+def show_homepage():
+    '''Deaf-friendly lost n' found homepage.'''
+
+@app.route('/login/')
+def show_login():
+    '''Deaf-friendly lost n' found login page.'''
+
+
+if __name__ == '__main__':
+    app.run()
